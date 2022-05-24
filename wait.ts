6fe81da43819484b68eabd37b30156e5ed26b61d
Louis <6723574+louisgv@users.noreply.github.com>
2022-05-23 22:48:48 -0400

    Migrate to utils, bump deps

---
+++ wait.ts
@@ -0,0 +1,19 @@
+import { stdout } from "process"
+
+import { cLog } from "./logging"
+
+export const wait = (duration: number) =>
+  new Promise((resolve) => setTimeout(resolve, duration))
+
+export async function exitCountDown(duration: number) {
+  for (let i = duration; i > 0; i--) {
+    stdout.write(
+      `${"🔴 EXIT".padEnd(
+        9
+      )} | This program will terminate in ${i} seconds...\r`
+    )
+    await wait(1000)
+  }
+  stdout.clearLine(0)
+  cLog("🔴 EXIT", `👋 Good bye and have a great day!`)
+}
