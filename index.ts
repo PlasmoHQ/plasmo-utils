99da3b03a160486458681c81c036c2f22915b2fa
Louis <6723574+louisgv@users.noreply.github.com>
2022-05-24 00:05:09 -0400

    Add fs utils

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from "./argv"
 export * from "./debounce"
 export * from "./flags"
+export * from "./fs"
 export * from "./logging"
 export * from "./platform"
 export * from "./transformer"
