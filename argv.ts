479f720ec553945992e385f34be9b3de571b15cd
Louis <6723574+louisgv@users.noreply.github.com>
2022-09-19 00:27:48 -0400

    Add default cmd

--- argv.ts
+++ argv.ts
@@ -1,4 +1,4 @@
-export const getNonFlagArgvs = (cmd: string) =>
+export const getNonFlagArgvs = (cmd = process.argv[2]) =>
   process.argv
     .filter((arg) => !arg.startsWith("--"))
     .slice(process.argv.indexOf(cmd) + 1)
