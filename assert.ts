49338838846f3d8ddb82387c849ff81880e19952
Louis <6723574+louisgv@users.noreply.github.com>
2022-10-04 20:59:47 -0400

    Adding more assert utils

--- assert.ts
+++ assert.ts
@@ -1,3 +1,14 @@
+import { AssertionError } from "assert"
+
 export function assertUnreachable(_value: never): never {
   throw new Error("Statement should be unreachable")
+}
+
+export function assertTruthy<T>(value: T, message = "Value is invalid") {
+  if (!value) {
+    throw new AssertionError({
+      message
+    })
+  }
+  return value
 }
